c068e89b96b086c2dab31cdd54740c41c7968687
Gyamjo Dong <nischallama222@gmail.com>
2023-05-26 18:37:01 +0545

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getFirestore, initializeFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDjZRSjiCMnIvFykhMOBOJvagKI8X-evc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
